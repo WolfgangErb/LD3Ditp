04753c3636453b387cbc0f6f87f90bbb7c924c1f
Wolfgang Erb <erb@math.uni-luebeck.de>
2017-11-02 21:46:05 -1000

    Update plotregion.m

--- plotregion.m
+++ plotregion.m
@@ -25,7 +25,7 @@ function plotregion(A,b,lb,ub,c,transp,points,linetyp,start_end)
 % If several regions must be plotted A ,b, lb, ub and c can be stored as a cell array {}
 % containing all sub-set information (see example1.m and example3.m). 
 %
-% Written by Per Bergstr�m 2006-01-16
+% Written by Per Bergström 2006-01-16
 
 
 if nargin<2
@@ -507,4 +507,31 @@ else      %  A is not a cell
     end
     
 end
+
+%License
+
+%Copyright (c) 2009, Per Bergström
+%All rights reserved.
+
+%Redistribution and use in source and binary forms, with or without
+%modification, are permitted provided that the following conditions are
+%met:
+
+%* Redistributions of source code must retain the above copyright
+%notice, this list of conditions and the following disclaimer.
+%* Redistributions in binary form must reproduce the above copyright
+%notice, this list of conditions and the following disclaimer in
+%the documentation and/or other materials provided with the distribution
+
+%THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS "AS IS"
+%AND ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE
+%IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE
+%ARE DISCLAIMED. IN NO EVENT SHALL THE COPYRIGHT OWNER OR CONTRIBUTORS BE
+%LIABLE FOR ANY DIRECT, INDIRECT, INCIDENTAL, SPECIAL, EXEMPLARY, OR
+%CONSEQUENTIAL DAMAGES (INCLUDING, BUT NOT LIMITED TO, PROCUREMENT OF
+%SUBSTITUTE GOODS OR SERVICES; LOSS OF USE, DATA, OR PROFITS; OR BUSINESS
+%INTERRUPTION) HOWEVER CAUSED AND ON ANY THEORY OF LIABILITY, WHETHER IN
+%CONTRACT, STRICT LIABILITY, OR TORT (INCLUDING NEGLIGENCE OR OTHERWISE)
+%ARISING IN ANY WAY OUT OF THE USE OF THIS SOFTWARE, EVEN IF ADVISED OF THE
+%POSSIBILITY OF SUCH DAMAGE.
 
